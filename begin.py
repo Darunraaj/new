4fec6c3f4b15db705c9bca1c5c26b8d6ef920376
Darunraaj <darunraaj16@gmail.com>
2024-05-26 15:24:36 +0530

    inserting rows

--- begin.py
+++ begin.py
@@ -3,6 +3,15 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "darun"
+    password = "darun",
+    database = "demo"
 )
-print(mydb)
+mycursor = mydb.cursor()
+sql = "insert into customers (customer_name , customer_age , address) values (%s,%s,%s)"
+val = [("darun", 21 , "TN"),
+       ("naveen" , 20 , "TN")]
+mycursor.executemany(sql , val)
+mydb.commit()
+print (mycursor.lastrowid, "record inserted.")
+#for db in mycursor:
+#    print (db)
